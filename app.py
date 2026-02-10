5d53a2fe4ba47975819ef68d1dbd002e4e40f488
GUTIERREZ-CANALES Fernando <fernando.canales@obspm.fr>
2026-02-10 20:30:59 +0100

    The buttons were replaced correctly in the app

--- app.py
+++ app.py
@@ -3,27 +3,29 @@ Docstring for app
 
 Created by Fernando on Feb. 10 2026
 """
-
 import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-scatter_button = st.button('Construir gráfico de dispersión')
-     
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-         
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-     
-    # mostrar un gráfico Plotly interactivo
+# leer los datos
+car_data = pd.read_csv('vehicles_us.csv')
+
+# crear casillas de verificación
+build_histogram = st.checkbox('Construir histograma')
+build_scatter = st.checkbox('Construir gráfico de dispersión')
+
+# histograma
+if build_histogram:
+    st.write(
+        'Creación de un histograma para el conjunto de datos '
+        'de anuncios de venta de coches'
+    )
+
+    fig = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig, use_container_width=True)
 
 # gráfico de dispersión
-if scatter_button:
+if build_scatter:
     st.write(
         'Creación de un gráfico de dispersión entre el odómetro '
         'y el precio de los coches'
@@ -31,8 +33,8 @@ if scatter_button:
 
     fig = px.scatter(
         car_data,
-        x="odometer",
-        y="price"
+        x='odometer',
+        y='price'
     )
 
-    st.plotly_chart(fig, use_container_width=True)     
+    st.plotly_chart(fig, use_container_width=True)
